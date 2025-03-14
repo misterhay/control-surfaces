520b69f424cdac07252b8bac5cc5ce0a8bf10043
David Hay <misterhay@gmail.com>
2025-03-14 11:15:34 -0600

    Update generate-numbers.py

--- generate-numbers.py
+++ generate-numbers.py
@@ -1,8 +1,9 @@
 import pyperclip
 import pyautogui
 
-prefix = '~9'
-words = 'TRANSITION FX KNOB'
+
+prefix = '~7'
+words = 'OVERLAY TXT KNOB'
 
 # make a list from 00 to FF
 hex_list = [f'{i:02X}' for i in range(256)]
@@ -14,7 +15,7 @@ for i, hex_string in enumerate(hex_list):
     pyperclip.copy(the_name)
     pyautogui.hotkey('ctrl', 'v')
     pyautogui.press('right')
-    pyperclip.copy(command)^F01F3
-    pyautogui.hotkey('ctrl', 'v'A x:1 y:244)^F01F4
+    pyperclip.copy(command)
+    pyautogui.hotkey('ctrl', 'v')
     pyautogui.press('down')
     pyautogui.press('left')
